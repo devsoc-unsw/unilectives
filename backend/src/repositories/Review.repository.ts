1776cd7b2a87364912abde81ead6b7b84cbbd7be
Joel Huang <miniginter@gmail.com>
2022-04-06 11:47:24 +1000

    Find reviews by course code working now

--- backend/src/repositories/Review.repository.ts
+++ backend/src/repositories/Review.repository.ts
@@ -10,24 +10,11 @@ export class ReviewRepository {
     courseCode: string
   ): Promise<ReviewEntity[]> {
       return await getRepository(ReviewEntity).find({
-        relations: ["reviews"],
         where: {
           courseCode 
         },
       });
   }
-  // async getCourseReviews(
-  //   courseCode: string
-  // ): Promise<ReviewEntity[]> {
-  //   const reviews = await getRepository(ReviewEntity)
-  //     .createQueryBuilder("reviews")
-  //     .where(
-  //       "reviews.course_code::text = :code",
-  //       { code: courseCode }
-  //     )
-  //     .getMany();
-  //   return reviews;
-  // }
 
   async postReview(
     review: ReviewEntity
