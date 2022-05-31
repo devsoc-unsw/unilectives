6ed1a6bd772b916d2da06814999821eeaf0dcc44
vivian <86246033+aquivere@users.noreply.github.com>
2022-05-31 16:41:26 +1000

    added terms and conditions page (#83)
    * added terms and conditions page

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
+import TCsPage from "./pages/TCsPage/TCsPage";
 
 const Router = () => (
   <Routes>
     <Route path="/" element={<HomePage />} />
+    <Route path="/termsandconditions" element={<TCsPage />} />
   </Routes>
 );
 
