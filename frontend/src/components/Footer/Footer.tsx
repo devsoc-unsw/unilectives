04a62e3c285401dba37d87bd91efb9efcec65e5f
Aimen Hamed <aimenhamed@live.com.au>
2022-06-12 15:19:21 +1000

    header and footer components

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import { CSESoc } from "../image/imageIndex";
+import { FooterWrapper, Logo } from "./style";
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <Logo src={CSESoc} />
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -4,6 +4,8 @@ import Text from "../../components/text/Text";
 import { Button, Container } from "./style";
 import { useAppSelector } from "src/logic/redux/hooks";
 import { selectUser } from "src/logic/redux/reducers/userSlice/userSlice";
+import Footer from "src/components/Footer/Footer";
+import Header from "src/components/Header/Header";
 
 const HomePage = () => {
   const [loginDialog, setLoginDialog] = useState<boolean>(false);
@@ -11,11 +13,13 @@ const HomePage = () => {
 
   return (
     <Container>
+      <Header />
       Hello1
       <Button onClick={() => setLoginDialog(true)}>Login</Button>
       {loginDialog && <LoginDialog close={() => setLoginDialog(false)} />}
       <Text>User response:</Text>
       <Text>{JSON.stringify(user)}</Text>
+      <Footer />
     </Container>
   );
 };
