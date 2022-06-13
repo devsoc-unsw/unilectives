c94dc31e8855b6974eeb04fab0a85f0cf6267633
Aimen <66828989+aimenhamed@users.noreply.github.com>
2022-06-13 10:22:21 +1000

    header and footer components (#87)
    * header and footer components
    
    * replaced header wave + position adjustments

---
+++ frontend/src/components/Footer/style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import { FooterWave } from "../image/imageIndex";
+
+export const FooterWrapper = styled.div`
+  background-image: url(${FooterWave});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position-y: 2rem;
+  display: flex;
+  padding: 2.3rem 2rem;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+`;
+
+export const Logo = styled.img`
+  margin-top: 2.75rem;
+`;
