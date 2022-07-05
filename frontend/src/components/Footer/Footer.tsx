444aeadccb61631e73d09590f1127d2121048c5a
Archit Mahajan <apmahajan10@gmail.com>
2022-07-05 23:31:24 +1000

    Added linking functionality to header and footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,10 +1,16 @@
 import { CSESoc } from "../image/imageIndex";
 import { FooterWrapper, Logo } from "./style";
+import {Routes, Route, useNavigate} from 'react-router-dom';
 
 const Footer = () => {
+  const navigate = useNavigate();
+  const navigateHome = () => {
+    navigate('/termsandconditions');
+  };
+
   return (
     <FooterWrapper>
-      <Logo src={CSESoc} />
+      <Logo src={CSESoc} onClick={navigateHome} />
     </FooterWrapper>
   );
 };
