bfba4b72dfa55e29602de5d8d204bbed2608d1a2
Archit Mahajan <apmahajan10@gmail.com>
2022-07-06 00:24:15 +1000

    linked header and footer components

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,10 +1,17 @@
 import { CSESoc } from "../image/imageIndex";
-import { FooterWrapper, Logo } from "./style";
+import { FooterWrapper, Logo, Link } from "./style";
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
 const Footer = () => {
+  const navigate = useNavigate();
+  const navigateHome = () => {
+    navigate('/termsandconditions');
+  };
+
   return (
     <FooterWrapper>
-      <Logo src={CSESoc} />
+      <Logo src={CSESoc}  />
+      <Link onClick={navigateHome}> Terms and Conditions </Link>
     </FooterWrapper>
   );
 };
--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,10 +1,16 @@
 import { HeaderWave, UniLectives } from "../image/imageIndex";
 import { Container, Logo, Wave } from "./style";
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const navigateHome = () => {
+    navigate('/');
+  };
+
   return (
     <Container>
-      <Logo src={UniLectives} />
+      <Logo src={UniLectives} onClick={navigateHome}/>
       <Wave src={HeaderWave} />
     </Container>
   );
