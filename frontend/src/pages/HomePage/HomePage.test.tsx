53e9a9c463d8167acb415417ec468d997bb5ad92
apm106 <apmahajan10@gmail.com>
2022-07-06 14:28:59 +1000

    Feature/elec408409v2 (#92)
    * linked header and footer components
    
    * Removed unused components from footer and header
    
    * fixed linking bug
    
    * removed unused imports
    
    * changed link to white
    
    Co-authored-by: vvwangg <vivianwang292@gmail.com>

--- frontend/src/pages/HomePage/HomePage.test.tsx
+++ frontend/src/pages/HomePage/HomePage.test.tsx
@@ -1,10 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
 import { render } from "src/helpers/rtl";
 import HomePage from "./HomePage";
 
 describe("<HomePage/>", () => {
   it("should render", () => {
-    const { getByText } = render(<HomePage />);
-
+    const { getByText } = render(<BrowserRouter><HomePage /></BrowserRouter>);
     expect(getByText("Login")).not.toBeNull();
   });
 });
