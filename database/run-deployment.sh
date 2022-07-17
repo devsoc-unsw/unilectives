df1e1428c0fef36a7ed28b5757e553529e7abc81
Aimen Hamed <aimenhamed@live.com.au>
2022-07-17 19:20:48 +1000

    debuging db script

--- database/run-deployment.sh
+++ database/run-deployment.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
+function check_env() {
+  eval val=\$$1
+  echo "Checking $1"
+  if [[ -z $val ]]; then
+    echo "$1 wasn't passed"
+    exit 1
+  fi
+}
+
+check_env POSTGRESQL_USER;
+check_env POSTGRESQL_PASSWORD;
+check_env POSTGRESQL_HOST;
+check_env POSTGRESQL_PORT;
+check_env POSTGRESQL_DATABASE;
+
 PGUSER="$POSTGRESQL_USER" PGPASSWORD="$POSTGRESQL_PASSWORD" sqitch --engine pg deploy --verify db:pg://$POSTGRESQL_HOST:$POSTGRESQL_PORT/$POSTGRESQL_DATABASE
+
