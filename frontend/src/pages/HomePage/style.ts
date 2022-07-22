b05c9b47a0db33678677674fa75513a4228e9da6
Joel Huang <miniginter@gmail.com>
2022-07-22 17:46:24 +1000

    Feature/elec 411 (#90)
    * added review converter test
    
    * added a few review.service tests
    
    * added the other missing service unit tests
    
    * frontend setup and svg added
    
    * assets added + draft svgs on homepage
    
    * background now legit styled div
    
    * random loading of different svg assets on homepage
    
    * welcome text component added
    
    * buttons and props for styled components
    
    * more svg choices and final cleanup before pr
    
    * removed trash and fixed lint errors
    
    * backend linting
    
    * npm run build error
    
    * lint test test lint fixes...
    
    * Header without wave for homepage
    
    * lint errors
    
    * small container stuff

--- frontend/src/pages/HomePage/style.ts
+++ frontend/src/pages/HomePage/style.ts
@@ -1,4 +1,13 @@
 import styled from "styled-components";
+import landingCurve from "src/assets/curves/landingCurve.svg";
+
+export const Content = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: url(${landingCurve});
+  background-size: cover;
+  background-position: top right;
+`;
 
 export const Container = styled.div`
   display: flex;
@@ -14,4 +23,94 @@ export const SmallContainer = styled.div`
   align-items: center;
 `;
 
-export const Button = styled.button``;
+export const HomeHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  padding-bottom: 10vh;
+`;
+
+export const Logo = styled.img`
+  margin-left: 5rem;
+  margin-top: 1rem;
+`;
+
+export const Graphic = styled.img`
+  max-width: 90%;
+
+  @media (max-width: 950px) {
+    display: none;
+  }
+`;
+
+export const Flexbox = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+interface FlexboxProps {
+  padding ?: string;
+  padding_top ?: string;
+  width ?: string;
+}
+
+export const FlexboxComponent = styled.div<FlexboxProps>`
+  justify-content: center;
+  align-items: center;
+  padding-left: ${props => props.padding};
+  padding-top: ${props => props.padding};
+  width: 50%;
+`;
+
+/** Landing Page CSS for Text  */
+export const TextFlexbox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+interface TextProps {
+  fontFamily ?: string;
+  fontWeight ?: string;
+  fontSize ?: string;
+  color ?: string;
+}
+
+export const HomeText = styled.text<TextProps>`
+  align-items: center;
+  text-align: start;
+  font-family: ${(props) => props.fontFamily}, sans-serif;
+  font-weight: ${(props) => props.fontWeight};
+  font-size: ${(props) => props.fontSize};
+  color: ${(props) => props.color};
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+`;
+
+/**Button CSS */
+export const ButtonFlexbox = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+`;
+
+interface ButtonProps {
+  bg ?: string;
+  color ?: string;
+}
+
+export const Button = styled.button<ButtonProps>`
+  border-radius: 20px;
+  border: none;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+  cursor: pointer;
+  font-size: 18px;
+  font-weight: 700;
+  padding: 15px 25px;
+  background-color: ${props => props.bg || '#333'};
+  color:  ${props => props.color || '#FFF'};
+
+  &:hover {
+    opacity: 0.9;
+    transform: scale(0.98);
+  }
+`;
