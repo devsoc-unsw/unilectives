ed25068517caa4bad1778145e09cf0ec2190daa6
Aimen Hamed <aimenhamed@live.com.au>
2022-09-01 19:23:26 +1000

    added search

--- frontend/src/components/image/imageIndex.ts
+++ frontend/src/components/image/imageIndex.ts
@@ -2,5 +2,14 @@ import CSESoc from "src/assets/images/csesoc.svg";
 import UniLectives from "src/assets/images/uni-lectives.svg";
 import FooterWave from "src/assets/images/footer-wave.svg";
 import HeaderWave from "src/assets/images/header-wave.svg";
+import ReviewGraphic from "src/assets/graphics/review.svg";
+import AlluraGraphic from "src/assets/graphics/allura.svg";
 
-export { HeaderWave, FooterWave, CSESoc, UniLectives };
+export {
+  HeaderWave,
+  FooterWave,
+  CSESoc,
+  UniLectives,
+  ReviewGraphic,
+  AlluraGraphic,
+};
---
+++ frontend/src/constants/index.ts
@@ -0,0 +1,3 @@
+import { AlluraGraphic, ReviewGraphic } from "src/components/image/imageIndex";
+
+export const homePageGraphics = [ReviewGraphic, AlluraGraphic];
