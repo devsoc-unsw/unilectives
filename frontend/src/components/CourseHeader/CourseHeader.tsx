87e10778b13ebfc4b556fcc67aca941cc672052c
Joel Huang <miniginter@gmail.com>
2022-10-19 23:50:36 +1100

    Course summary component (#122)
    * Implemented course review header with hard-coded data
    
    * header and footer components (#87)
    
    * header and footer components
    
    * replaced header wave + position adjustments
    
    * Added header to course review page
    
    * setup of components for review cards
    
    * commit to pull
    
    * course summary component with terms, rating and rating fields so far
    
    * styling of course summary component
    
    * change course header to figma design
    
    * calling the mock backend + css fixes
    
    * commit before merging dev
    
    * lint fixes
    
    * fix footer
    
    * sticky footer changes
    
    Co-authored-by: vvwangg <vivianwang292@gmail.com>
    Co-authored-by: Aimen <66828989+aimenhamed@users.noreply.github.com>

--- frontend/src/components/CourseHeader/CourseHeader.tsx
+++ frontend/src/components/CourseHeader/CourseHeader.tsx
@@ -1,9 +1,11 @@
-import { Container, CourseCodeBox, CourseCode, CourseName } from "./style";
-import { OverallBox, OverallRow, OverallStars, NumReviews } from "./style";
-import { AllRatingsBox, RatingBox, RatingTitle, RatingNum, OutOfRating, CategoryRating } from "./style";
-
-import { Rating } from '@mui/material';
-import { palette } from "../palette/palette";
+import {
+  Container,
+  CourseCodeBox,
+  CourseCode,
+  CourseName,
+  Button,
+  Text,
+} from "./style";
 
 type CourseHeaderProps = {
   courseCode: string,
@@ -14,61 +16,22 @@ type CourseHeaderProps = {
   noReviews: number,
 }
 
-const CourseHeader = ({ courseCode,
-                        overallRating,
-                        enjoyabilityRating,
-                        usefulnessRating,
-                        manageabilityRating,
-                        noReviews,
-                      }: CourseHeaderProps) => {
-    return (
-      <Container>
-        <CourseCodeBox>
-          <CourseCode>{courseCode}</CourseCode>
-          <CourseName>Data Structures and Algorithms</CourseName>
-        </CourseCodeBox>
-        <OverallBox>
-          <Rating
-            name="read-only"
-            value={overallRating}
-            precision={0.1}
-            size="large"
-            sx={{
-              color: palette.dayGreen,
-              fontSize: "3em",
-            }}
-            readOnly
-          />
-          <OverallRow>
-            <OverallStars>{overallRating} stars</OverallStars>
-            <NumReviews>{noReviews} reviews</NumReviews>
-          </OverallRow>
-        </OverallBox>
-        <AllRatingsBox>
-          <RatingBox>
-            <RatingTitle>Enjoyability</RatingTitle>
-            <CategoryRating>
-              <RatingNum>{enjoyabilityRating}</RatingNum>
-              <OutOfRating>/5</OutOfRating>
-            </CategoryRating>
-          </RatingBox>
-          <RatingBox>
-            <RatingTitle>Usefulness</RatingTitle>
-            <CategoryRating>
-              <RatingNum>{usefulnessRating}</RatingNum>
-              <OutOfRating>/5</OutOfRating>
-            </CategoryRating>
-          </RatingBox>
-          <RatingBox>
-            <RatingTitle>Manageability</RatingTitle>
-            <CategoryRating>
-              <RatingNum>{manageabilityRating}</RatingNum>
-              <OutOfRating>/5</OutOfRating>
-            </CategoryRating>
-          </RatingBox>
-        </AllRatingsBox>
-      </Container>
-    );
+const CourseHeader = ({ courseCode, }: CourseHeaderProps) => {
+
+  return (
+    <Container>
+      <CourseCodeBox>
+        <CourseCode>{courseCode}</CourseCode>
+        <CourseName>Data Structures and Algorithms</CourseName>
+      </CourseCodeBox>
+      {/* TODO: Review Modal Popup */}
+      <Button bg={'#8A96E4'}>
+        <Text color={'#FFF'} fontSize={'18px'}>
+          Add a Review
+        </Text>
+      </Button>
+    </Container>
+  );
   };
 
 
---
+++ frontend/src/components/CourseSummary/CourseSummary.tsx
@@ -0,0 +1,121 @@
+import { useState, useEffect } from "react";
+import {
+  Link
+} from "react-router-dom";
+import { Container, CourseTerms, Text, Flexbox } from "./style";
+import { AllRatingsBox, RatingBox, RatingTitle, RatingNum, OutOfRating, CategoryRating } from "./style";
+import Rating from '@mui/material/Rating';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import { mockCourses } from '../../stubbing/data'
+
+type CourseSummaryProps = {
+  course: string,
+  enjoyabilityRating: number,
+  usefulnessRating: number,
+  manageabilityRating: number,
+  noReviews: number,
+}
+
+const CourseSummary = ({course,
+                        enjoyabilityRating,
+                        usefulnessRating,
+                        manageabilityRating,
+                        noReviews,
+                      }: CourseSummaryProps) => {
+
+  // TODO: Set value for rating through logic
+  const [rating, setRating] = useState<number | null>(5);
+
+  const [clamped, setClamped] = useState<boolean>(true);
+  const [showButton, setShowButton] = useState<boolean>(true);
+
+  const [courseTerms, setCourseTerms] = useState<number[]>([]);
+  const [courseDesc, setCourseDesc] = useState<string>();
+
+  const handleClick = () => {
+    setClamped(!clamped);
+    setShowButton(!showButton);
+  }
+
+  useEffect(() => {
+    const result = mockCourses.find((obj) => {
+      return obj.courseCode === course;
+    });
+    setCourseDesc(result.description);
+    // console.log(typeof(result.terms));
+    setCourseTerms(result.terms);
+    setRating(result.rating);
+  }, [course]);
+
+
+  return (
+    <Container>
+      <Flexbox>
+        {courseTerms.map((term: any) => (
+          <CourseTerms key={term}>
+            <Text fontFamily={"Roboto"} fontWeight={"regular"} fontSize="15px">
+              Term {term}
+            </Text>
+          </CourseTerms>
+        ))}
+      </Flexbox>
+      <Link color={'#38B2E5'} target="_blank" to={`//www.handbook.unsw.edu.au/undergraduate/courses/2023/${course}`}>
+        <Flexbox>
+          <OpenInNewIcon style={{ fill: '#38B2E5' }} />
+          {course} Handbook Page
+        </Flexbox>
+      </Link>
+      <Flexbox>
+        <Rating
+          name="read-only"
+          value={rating}
+          readOnly
+          sx={{
+            '& .MuiRating-iconFilled': {
+              color: '#326BF7',
+              fontSize: '1.7rem',
+            },
+          }}
+        />
+        <Text color={'#808080'}>
+          {noReviews} reviews
+        </Text>
+      </Flexbox>
+      <AllRatingsBox>
+        <RatingBox>
+          <RatingTitle>Enjoyability</RatingTitle>
+          <CategoryRating>
+            <RatingNum>{enjoyabilityRating}</RatingNum>
+            <OutOfRating>/5</OutOfRating>
+          </CategoryRating>
+        </RatingBox>
+        <RatingBox>
+          <RatingTitle>Usefulness</RatingTitle>
+          <CategoryRating>
+            <RatingNum>{usefulnessRating}</RatingNum>
+            <OutOfRating>/5</OutOfRating>
+          </CategoryRating>
+        </RatingBox>
+        <RatingBox>
+          <RatingTitle>Manageability</RatingTitle>
+          <CategoryRating>
+            <RatingNum>{manageabilityRating}</RatingNum>
+            <OutOfRating>/5</OutOfRating>
+          </CategoryRating>
+        </RatingBox>
+      </AllRatingsBox>
+      <Text fontWeight={"bold"}>
+        Course Description
+      </Text>
+      <Text>
+        {courseDesc}
+      </Text>
+
+      {showButton && (
+        <Text onClick={handleClick}>Read {clamped ? "more" : "less"}</Text>
+      )}
+    </Container>
+  );
+}
+
+export default CourseSummary;
