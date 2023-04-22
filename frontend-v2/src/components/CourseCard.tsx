df54065918f25bc26039ca11ca2eb272c3ba4288
Merry Rosalie <z5391208@ad.unsw.edu.au>
2023-04-22 14:25:31 +1000

    chore: cclean up lint issues and unneccessary star svg uploads

--- frontend-v2/src/components/CourseCard.tsx
+++ frontend-v2/src/components/CourseCard.tsx
@@ -24,56 +24,56 @@ const CourseCard = ({
   );
 
   return (
-    <div className='box-border px-6 py-7 bg-unilectives-card shadow-card rounded-xl space-y-2'>
+    <div className="box-border px-6 py-7 bg-unilectives-card shadow-card rounded-xl space-y-2">
       {/* Course code + Ratings */}
-      <div className='flex flex-wrap justify-between text-2xl'>
+      <div className="flex flex-wrap justify-between text-2xl">
         {/* Rating */}
-        <h1 className='font-bold'>{code}</h1>
-        <div className='relative text-2xl text-right text-unilectives-subheadings'>
-          <span aria-label='rating' className=''>
+        <h1 className="font-bold">{code}</h1>
+        <div className="relative text-2xl text-right text-unilectives-subheadings">
+          <span aria-label="rating" className="">
             ★★★★★
           </span>
           <span
-            className='absolute inset-0 bg-unilectives-purple text-transparent bg-clip-text'
+            className="absolute inset-0 bg-unilectives-purple text-transparent bg-clip-text"
             style={{ width: `${percentage}%` }}
             aria-hidden={true}
           >
             ★★★★★
           </span>
           {/* Number of reviews */}
-          <p className='text-xs'>
+          <p className="text-xs">
             {/* Format number to their abbreviated string e.g 1000 to 1k, or 1000000 to 1M */}
-            {Intl.NumberFormat('en-US', {
-              notation: 'compact',
+            {Intl.NumberFormat("en-US", {
+              notation: "compact",
               maximumFractionDigits: 1,
-            }).format(reviews)}{' '}
+            }).format(reviews)}{" "}
             reviews
           </p>
         </div>
       </div>
       {/* Course name */}
-      <p className='text-sm text-unilectives-headings h-16 break-all line-clamp-3'>
+      <p className="text-sm text-unilectives-headings h-16 break-all line-clamp-3">
         {name}
       </p>
       {/* Terms */}
-      <div className='flex flex-wrap gap-2'>
+      <div className="flex flex-wrap gap-2">
         {terms.summer && (
-          <span className='bg-unilectives-tags text-xs py-1 px-2 rounded-full'>
+          <span className="bg-unilectives-tags text-xs py-1 px-2 rounded-full">
             Summer
           </span>
         )}
         {terms.term1 && (
-          <span className='bg-unilectives-tags text-xs py-1 px-2 rounded-full'>
+          <span className="bg-unilectives-tags text-xs py-1 px-2 rounded-full">
             Term 1
           </span>
         )}
         {terms.term2 && (
-          <span className='bg-unilectives-tags text-xs py-1 px-2 rounded-full'>
+          <span className="bg-unilectives-tags text-xs py-1 px-2 rounded-full">
             Term 2
           </span>
         )}
         {terms.term3 && (
-          <span className='bg-unilectives-tags text-xs py-1 px-2 rounded-full'>
+          <span className="bg-unilectives-tags text-xs py-1 px-2 rounded-full">
             Term 3
           </span>
         )}
