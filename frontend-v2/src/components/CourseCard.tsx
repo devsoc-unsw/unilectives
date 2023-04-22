5d38d6706e66113b719caf79fd5af7043b9771a5
Merry Rosalie <z5391208@ad.unsw.edu.au>
2023-04-22 14:22:06 +1000

    fix: slight accessibility improvements on the components

--- frontend-v2/src/components/CourseCard.tsx
+++ frontend-v2/src/components/CourseCard.tsx
@@ -19,23 +19,29 @@ const CourseCard = ({
   terms,
 }: CourseCardProps) => {
   // Convert percentage to 1 decimal float
-  const percentage = parseFloat(((rating / 5) * 100).toFixed(1));
+  const percentage = parseFloat(
+    (((rating > 5 ? 5 : rating) / 5) * 100).toFixed(1)
+  );
 
   return (
     <div className='box-border px-6 py-7 bg-unilectives-card shadow-card rounded-xl space-y-2'>
       {/* Course code + Ratings */}
-      <div className='flex flex-wrap justify-between'>
-        <h1 className='text-2xl font-bold'>{code}</h1>
+      <div className='flex flex-wrap justify-between text-2xl'>
         {/* Rating */}
-        <div className='text-2xl relative text-right'>
-          <span className='text-unilectives-subheadings'>★★★★★</span>
+        <h1 className='font-bold'>{code}</h1>
+        <div className='relative text-2xl text-right text-unilectives-subheadings'>
+          <span aria-label='rating' className=''>
+            ★★★★★
+          </span>
           <span
             className='absolute inset-0 bg-unilectives-purple text-transparent bg-clip-text'
             style={{ width: `${percentage}%` }}
+            aria-hidden={true}
           >
             ★★★★★
           </span>
-          <p className='text-xs text-unilectives-subheadings'>
+          {/* Number of reviews */}
+          <p className='text-xs'>
             {/* Format number to their abbreviated string e.g 1000 to 1k, or 1000000 to 1M */}
             {Intl.NumberFormat('en-US', {
               notation: 'compact',
@@ -52,24 +58,24 @@ const CourseCard = ({
       {/* Terms */}
       <div className='flex flex-wrap gap-2'>
         {terms.summer && (
-          <div className='bg-unilectives-tags text-xs py-1 px-2 rounded-full'>
+          <span className='bg-unilectives-tags text-xs py-1 px-2 rounded-full'>
             Summer
-          </div>
+          </span>
         )}
         {terms.term1 && (
-          <div className='bg-unilectives-tags text-xs py-1 px-2 rounded-full'>
+          <span className='bg-unilectives-tags text-xs py-1 px-2 rounded-full'>
             Term 1
-          </div>
+          </span>
         )}
         {terms.term2 && (
-          <div className='bg-unilectives-tags text-xs py-1 px-2 rounded-full'>
+          <span className='bg-unilectives-tags text-xs py-1 px-2 rounded-full'>
             Term 2
-          </div>
+          </span>
         )}
         {terms.term3 && (
-          <div className='bg-unilectives-tags text-xs py-1 px-2 rounded-full'>
+          <span className='bg-unilectives-tags text-xs py-1 px-2 rounded-full'>
             Term 3
-          </div>
+          </span>
         )}
       </div>
     </div>
