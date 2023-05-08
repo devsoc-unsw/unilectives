97b6306587b907e13b6807489d367c011bef4aec
Merlte <wstly92@gmail.com>
2023-05-08 18:51:06 +1000

    Fix: jest

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json"
-    }
-  },
   moduleFileExtensions: ["js", "json", "ts"],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+    "^.+\\.(ts|tsx)$": ["ts-jest", {
+      tsconfig: 'tsconfig.json',
+    }]
   },
   testMatch: ["**/*.test.(ts|js)"],
   testEnvironment: "node",
