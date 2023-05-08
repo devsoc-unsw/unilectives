e955dfa3abfb59c46bae689fa9f36a54948b22b7
Dylan ZW <inutuzumab@gmail.com>
2023-05-08 16:07:53 +1000

    Working on functionality

--- frontend-v2/src/app/page.tsx
+++ frontend-v2/src/app/page.tsx
@@ -13,3 +13,17 @@ export default function Home() {
     </Content>
   );
 }
+
+export async function getServerSideProps() {
+  const response = await fetch('localhost:3030/api/v1/courses', {
+    method: 'GET'
+  });
+  const data = await response.json();
+
+  return {
+    props: {
+      courses: data,
+      reviews: data
+    }
+  }
+}
