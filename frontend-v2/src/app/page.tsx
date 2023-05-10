894f431c4a9c61f347b8a374f01cd2bdb73e7b23
Dylan ZW <inutuzumab@gmail.com>
2023-05-11 03:02:34 +1000

    Finished static page.

--- frontend-v2/src/app/page.tsx
+++ frontend-v2/src/app/page.tsx
@@ -5,20 +5,15 @@ import getAllCourses from "../../lib/getAllCourses";
 import CourseCard from "../components/CourseCard";
 
 export default async function Home() {
-  const retrieveCourses: Promise<Course[]> = getAllCourses();
-  const courses: Course[] = await retrieveCourses;
-  console.log(courses);
-
+  const courses: Course[] = await getAllCourses();
   return (
     <Content>
       {/* Logo */}
       <div className="flex">
         <img className="pt-[1rem] pl-[5rem]" src="/images/uni-lectives.svg" />
       </div>
-      {/* Landing screen main container */}      
+      {/* HomeText */}
       <div className="flex flex-row justify-between">
-        
-        {/* Home text */}
         <div className="justify-center items-center pl-0 w-full pt-standard">
           <div className="flex flex-row w-full justify-between gap-[10px] mt-[15em]">
             <div className="flex flex-col gap-[10px] ml-[18em]">
@@ -32,8 +27,7 @@ export default async function Home() {
                 Your one-stop shop for UNSW course and elective reviews.
               </p>
             </div>
-            {/* 'Add a Review' button */}
-            <div className="flex flex-row gap-px items-center justify-center mr-[18em]">
+            <div className="flex flex-row mr-0 gap-px items-center justify-center mr-[18em]">
               <button className="px-[28px] py-[13px] rounded-3xl bg-unilectives_blue hover:bg-unilectives_blue2 hover:scale-[0.98]">
                 <p className="whitespace-no-wrap items-center drop-shadow justify-center font-lato text-white font-semibold">
                   Add a Review
@@ -43,23 +37,44 @@ export default async function Home() {
           </div>
         </div>
       </div>
-      {/* Search bar */}
-      <div className="flex flex-col space-between gap-[5em]">
-        <div className="flex overflow-hidden flex-row mt-[2em] ml-[18em] mr-[18em] rounded-full border-2 border-unilectives_blue">
+      {/* Searchbar Container */}
+      <div className="flex flex-col space-between">
+        {/* Searchbar */}
+        <div className="flex overflow-hidden flex-row mt-[2em] ml-[18em] mr-[18em] rounded-xl border-2 border-unilectives_blue">
           <section className="flex flex-row space-between w-full h-10">
             <span className="w-[3em] h-[3em]">
-              <img className="p-[0.8em]" src="/images/search-icon.svg" />
+              <img className="p-[0.9em]" src="/images/search-icon.svg" />
             </span>
             <input type="text" className="w-full h-full bg-slate-0 placeholder:italic placeholder-unilectives_blue focus:outline-none"
               placeholder="Search for a course e.g. COMP1511"></input>
           </section>
         </div>
+        {/* Filter Options */}
+        <div className="flex flex-row justify-between mt-[0.5em] mx-[19em]">
+          <div className="flex flex-row space-between gap-[2em]">
+            Sort by:
+            <button type="button" className="px-[2em] inline-flex flex-row space-between jsutify-center gap-[1em]">
+              Most Reviews
+              <img className="object-scale-down h-[1.5rem] w-[1.5rem]" src="/images/dropdown.svg" />
+            </button>
+          </div>
+          <div className="flex flex-row space-between gap-[2em]">
+            <button type="button" className="px-[2em] inline-flex flex-row space-between jsutify-center gap-[1em]">
+                Faculty
+                <img className="object-scale-down h-[1.5rem] w-[1.5rem]" src="/images/dropdown.svg" />
+            </button>
+            <button type="button" className="px-[2em] inline-flex flex-row space-between jsutify-center gap-[1em]">
+              Term
+              <img className="object-scale-down h-[1.5rem] w-[1.5rem]" src="/images/dropdown.svg" />
+            </button>
+          </div>
+        </div>
         {/* Course Cards */}
-        {/* <div className="grid grid-cols-responsive-cards gap-4 pl-[18em] pr-[18em]">
-          {courses.slice(0, 10).map(course => (
-            <CourseCard name="{course.fact}" code="x" rating={2} numReviews={1}/>
-          ))};
-        </div> */}
+        <div className="grid grid-cols-responsive-cards gap-4 pl-[18em] pr-[18em] mt-[4em]">
+            {courses.slice(0,4).map(course => (
+                <CourseCard name={course.title} code={course.title} rating={1} numReviews={2} />
+            ))};
+        </div>
       </div>
     </Content>
   );
--- frontend-v2/type.d.ts
+++ frontend-v2/type.d.ts
@@ -1,44 +1,51 @@
 type Course = {
-    courseCode: string;
-    archived: boolean;
-    attributes: string[];
-    calendar: string;
-    campus: string;
-    description: string;
-    enrolmentRules: string;
-    equivalents: string[];
-    exclusions: string[];
-    faculty: string;
-    fieldOfEducation: string;
-    genEd: boolean;
-    level: number;
-    school: string;
-    studyLevel: string;
-    terms: number[];
-    title: string;
-    uoc: number;
-    rating: number; // same as overallRating
-    reviewCount: number;
-    overallRating: number;
-    manageability: number;
-    usefulness: number;
-    enjoyability: number;
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean
 }
+
+// type Course = {
+//     courseCode: string;
+//     archived: boolean;
+//     attributes: string[];
+//     calendar: string;
+//     campus: string;
+//     description: string;
+//     enrolmentRules: string;
+//     equivalents: string[];
+//     exclusions: string[];
+//     faculty: string;
+//     fieldOfEducation: string;
+//     genEd: boolean;
+//     level: number;
+//     school: string;
+//     studyLevel: string;
+//     terms: number[];
+//     title: string;
+//     uoc: number;
+//     rating: number; // same as overallRating
+//     reviewCount: number;
+//     overallRating: number;
+//     manageability: number;
+//     usefulness: number;
+//     enjoyability: number;
+// }
   
-// Reviews
-type Review = {
-    reviewId: string;
-    zid: string;
-    courseCode: string;
-    authorName: string;
-    description: string;
-    grade: number;
-    termTaken: string;
-    createdTimestamp: string;
-    updatedTimestamp: string;
-    upvotes: string[];
-    manageability: number;
-    enjoyability: number;
-    usefulness: number;
-    overallRating: number;
-}
+// // Reviews
+// type Review = {
+//     reviewId: string;
+//     zid: string;
+//     courseCode: string;
+//     authorName: string;
+//     description: string;
+//     grade: number;
+//     termTaken: string;
+//     createdTimestamp: string;
+//     updatedTimestamp: string;
+//     upvotes: string[];
+//     manageability: number;
+//     enjoyability: number;
+//     usefulness: number;
+//     overallRating: number;
+// }
