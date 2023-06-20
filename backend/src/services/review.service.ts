97da7231c5415ba9ed80fa2b6d8e89536a9ac0b7
vinsentcs <107679335+vinsentcs@users.noreply.github.com>
2023-06-20 23:49:02 +1000

    feat(backend): add cache logic to GET /reviews/:courseCode api (#208)
    * feat: added cache logic to get reviews/:courseCode API
    
    * fix: fixed review service tests

--- backend/src/services/review.service.ts
+++ backend/src/services/review.service.ts
@@ -39,12 +39,17 @@ export class ReviewService {
   async getCourseReviews(
     courseCode: string
   ): Promise<ReviewsSuccessResponse | undefined> {
-    const reviews: ReviewEntity[] =
-      await this.reviewRepository.getCourseReviews(courseCode);
-    if (reviews.length === 0) {
-      this.logger.error("Database returned with no reviews.");
-      throw new HTTPError(internalServerError);
+    let reviews = await this.redis.get<ReviewEntity[]>(`reviews:${courseCode}`);
+
+    if (!reviews) {
+      this.logger.info(`Cache miss on reviews:${courseCode}`);
+      reviews = await this.reviewRepository.getCourseReviews(courseCode);
+      await this.redis.set(`reviews:${courseCode}`, reviews);
+    } else {
+      this.logger.info(`Cache hit on reviews:${courseCode}`);
     }
+
+    this.logger.info(`Found ${reviews.length} reviews.`);
     return {
       reviews: reviews.map((review) => {
         return {
