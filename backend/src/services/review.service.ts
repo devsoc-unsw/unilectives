f4f131e48f644caeef1f69e15e11521246efed84
Merlte <wstly92@gmail.com>
2023-06-22 21:22:40 +1000

    Fix: postReview test mock db

--- backend/src/services/review.service.ts
+++ backend/src/services/review.service.ts
@@ -21,7 +21,7 @@ export class ReviewService {
   constructor(
     private readonly manager: EntityManager,
     private readonly redis: RedisClient
-  ) {}
+  ) { }
   private reviewRepository = new ReviewRepository(this.manager);
   private userRepository = new UserRepository(this.manager);
 
@@ -75,10 +75,7 @@ export class ReviewService {
 
     const review = await this.reviewRepository.save(reviewEntity);
 
-    let reviews = await this.redis.get<ReviewEntity[]>(
-      `reviews:${reviewDetails.courseCode}`
-    );
-    reviews = await this.reviewRepository.getCourseReviews(
+    let reviews = await this.reviewRepository.getCourseReviews(
       reviewDetails.courseCode
     );
     await this.redis.set(`reviews:${reviewDetails.courseCode}`, reviews);
@@ -159,10 +156,8 @@ export class ReviewService {
     user = await this.userRepository.saveUser(user);
 
     this.logger.info(
-      `Successfully ${
-        reviewDetails.bookmark ? "bookmarked" : "removed bookmarked"
-      } review with reviewId ${reviewDetails.reviewId} for user with zID ${
-        reviewDetails.zid
+      `Successfully ${reviewDetails.bookmark ? "bookmarked" : "removed bookmarked"
+      } review with reviewId ${reviewDetails.reviewId} for user with zID ${reviewDetails.zid
       }.`
     );
     return {
@@ -191,10 +186,8 @@ export class ReviewService {
     review = await this.reviewRepository.save(review);
 
     this.logger.info(
-      `Successfully ${
-        upvoteDetails.upvote ? "upvoted" : "removed upvote from"
-      } review with reviewId ${upvoteDetails.reviewId} for user with zID ${
-        upvoteDetails.zid
+      `Successfully ${upvoteDetails.upvote ? "upvoted" : "removed upvote from"
+      } review with reviewId ${upvoteDetails.reviewId} for user with zID ${upvoteDetails.zid
       }.`
     );
 
