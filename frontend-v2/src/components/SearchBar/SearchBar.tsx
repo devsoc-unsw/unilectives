766c3da16b3a0f9f08cc69146ca22a11df057e04
Edward Lukman <z5419884@ad.unsw.edu.au>
2023-07-06 23:57:23 +1000

    adding filter selection icon

--- frontend-v2/src/components/SearchBar/SearchBar.tsx
+++ frontend-v2/src/components/SearchBar/SearchBar.tsx
@@ -1,29 +1,45 @@
 "use client";
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
-import { FormEvent } from "react";
+import { AdjustmentsVerticalIcon } from "@heroicons/react/24/outline";
+import { FormEvent, useState } from "react";
+import FilterSelection from "../FilterSelection/FilterSelection";
 
 export default function SearchBar() {
+  const [isFilterOpen, setIsFilterOpen] = useState(false);
+
   const handleOnSubmit = (event: FormEvent) => {
     event.preventDefault();
     // TODO: submit query
     console.log((event.target as HTMLFormElement).query.value);
   };
 
+  const handleFilterClick = () => {
+    setIsFilterOpen(true);
+  };
+
   return (
-    <form
-      className="flex w-5/6 items-center bg-white rounded border-2 border-unilectives-search"
-      onSubmit={handleOnSubmit}
-    >
-      <button type="submit">
-        <MagnifyingGlassIcon className="w-6 h-6 text-unilectives-search mx-2" />
+    <div className="flex w-5/6 items-center bg-white rounded border-2 border-unilectives-search">
+      <form className="flex w-full" onSubmit={handleOnSubmit}>
+        <button type="submit">
+          <MagnifyingGlassIcon className="w-6 h-6 text-unilectives-search mx-2" />
+        </button>
+        <input
+          type="text"
+          name="query"
+          className="w-full py-2 px-3 text-sm text-unilectives-search focus:outline-none placeholder-unilectives-search font-medium"
+          placeholder="Search for a course e.g. COMP1511"
+        />
+      </form>
+      <button
+        className={`p-2 bg-transparent`}
+        onClick={handleFilterClick}
+      >
+        <AdjustmentsVerticalIcon className="w-6 h-6 text-unilectives-search" />
       </button>
-      <input
-        type="text"
-        name="query"
-        className="w-full py-2 px-3 text-sm text-unilectives-search focus:outline-none placeholder-unilectives-search font-medium"
-        placeholder="Search for a course e.g. COMP1511"
-      />
-    </form>
+      {/* Filter selection */}
+      {isFilterOpen && <FilterSelection onClose={() => setIsFilterOpen(false)} />}
+    </div>
   );
 }
+
