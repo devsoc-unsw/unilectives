269d7503ef7a02f8c9883d83c8008d2d40e6d037
Dylan W <inutuzumab@gmail.com>
2023-07-14 01:51:27 +1000

    feature/ELEC-499: added responsiveness

--- frontend-v2/src/app/layout.tsx
+++ frontend-v2/src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
     <html lang="en" className="font-custom">
       <body>
         <Navbar />
-        <div className="ml-72">{children}</div>
+        <div className="ml-72 lg:ml-20">{children}</div>
       </body>
     </html>
   );
--- frontend-v2/src/components/Navbar/Navbar.tsx
+++ frontend-v2/src/components/Navbar/Navbar.tsx
@@ -5,9 +5,9 @@ import { BookOpenIcon, PencilSquareIcon, ShieldCheckIcon, UserCircleIcon, BarsAr
 
 export default function Navbar() {
 	return (
-		<div className="fixed flex flex-col h-screen w-72 gap-4 p-4 bg-gray-50">
+		<div className="fixed flex flex-col h-screen w-72 gap-4 p-4 bg-gray-50 lg:w-20">
 			{/* Logo */}
-			<div className="flex flex-row items-center justify-between h-10 p-3">
+			<div className="flex flex-row items-center justify-between h-10 p-2">
 				<Link href="/">
 					<Image
 						src={logo}
@@ -17,42 +17,42 @@ export default function Navbar() {
 						priority
 					/>
 				</Link>
-				<p className="text-xl font-semibold">Uni-lectives</p>
-				<BarsArrowDownIcon title="Minimise " className="w-12 h-12 p-3 rotate-90 hover:bg-slate-200 rounded-xl" />
+				<p className="text-xl font-semibold lg:hidden">Uni-lectives</p>
+				<BarsArrowDownIcon title="Minimise" className="w-12 h-12 p-3 rotate-90 hover:bg-slate-200 rounded-xl lg:hidden" />
 			</div>
-			{/* Toolkit Container */}
+			{/* Navbar Container */}
 			<div className="flex flex-col h-full w-full justify-between border-t-2 border-gray-200">
 				{/* Review Options */}
 				<div className="flex flex-col gap-3 items-left py-3">
 					<Link className="flex flex-row items-center hover:bg-slate-200 rounded-xl" href="/">
 						<BookOpenIcon className="w-12 h-12 p-3" />
-						<p>Browse Courses</p>
+						<p className="lg:hidden">Browse Courses</p>
 					</Link>
 					<Link className="flex flex-row items-center hover:bg-slate-200 rounded-xl" href="/user/zid">
 						<PencilSquareIcon className="w-12 h-12 p-3 hover:bg-slate-200 rounded-xl" />
-						<p>My Reviews</p>
+						<p className="lg:hidden">My Reviews</p>
 					</Link>					
 					<Link className="flex flex-row items-center hover:bg-slate-200 rounded-xl" href="/terms-and-conditions">
 						<ShieldCheckIcon className="w-12 h-12 p-3 hover:bg-slate-200 rounded-xl" />
-						<p>Terms and Conditions</p>
+						<p className="lg:hidden">Terms and Conditions</p>
 					</Link>
 				</div>
 				{/* Account Options */}
 				<div className="flex flex-col gap-3 py-2 items-left">
 					{/* link to `/user/{zID} */}
-					<div className="flex flex-row justify-between gap-2">
+					<div className="flex flex-row justify-between gap-2 lg:flex-col">
 						<Link className="flex flex-row w-full items-center hover:bg-slate-200 rounded-xl" href="user/zid">
 							<UserCircleIcon className="w-12 h-12 p-3" />
-							<p>Name</p>
+							<p className="lg:hidden">Name</p>
 						</Link>
 						<MoonIcon title="Dark Mode" className="w-12 h-12 p-3 bg-transparent hover:bg-slate-200 rounded-xl" />
 					</div>
 					<Link className="flex flex-row items-center justify-center hover:text-red-600 hover:bg-red-100 rounded-xl gap-2" href="/logout">
 						<ArrowRightOnRectangleIcon className="w-6 h-12 py-3" />
-						<p>Logout</p>
+						<p className="lg:hidden">Logout</p>
 					</Link>
-					<p className="text-xs">By using this site, you agree to the <a href="/terms-and-conditions" className="inline text-blue-500 hover:underline">terms and conditions</a></p>
-					<p className="text-xs">© CSESoc 2023, v1.0.0</p>
+					<p className="text-xs lg:hidden">By using this site, you agree to the <a href="/terms-and-conditions" className="inline text-blue-500 hover:underline lg:hidden">terms and conditions</a></p>
+					<p className="text-xs lg:hidden">© CSESoc 2023, v1.0.0</p>
 				</div>
 			</div>
 		</div>
