8f5b6bd3360b797bde44d138b87a806874755a0d
Vincent <vincentle0402@hotmail.com>
2023-07-18 19:11:59 +1000

    search dropdown for course page

--- frontend-v2/src/components/ReviewSearchBar/ReviewSearchBar.tsx
+++ frontend-v2/src/components/ReviewSearchBar/ReviewSearchBar.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { Course, Courses } from "@/types/api";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import { debounce } from "lodash";
-import { ChangeEvent, useCallback, useState } from "react";
+import { ChangeEvent, useCallback, useEffect, useState } from "react";
+import { get } from "@/utils/request";
 
 export default function ReviewSearchbar() {
+  // States
   const [searchTerm, setSearchTerm] = useState("");
+  const [allCourses, setAllCourses] = useState<Course[]>([]);
 
   const handleOnChange = useCallback(
     debounce((event: ChangeEvent<HTMLInputElement>) => {
@@ -13,21 +17,47 @@ export default function ReviewSearchbar() {
     }, 300), []
   );
 
+  useEffect(() => {
+    const getSearchResults = async () => {
+      try {
+        if (searchTerm) {
+          const { courses } = (await get(`/course/search/${searchTerm}`)) as Courses;
+          setAllCourses(courses.slice(0, 25));
+        } else {
+          setAllCourses([]);
+        }
+      } catch (err) {
+        console.log(`error fetching courses from /course/search/${searchTerm}`);
+      }
+    };
+    getSearchResults();
+  }, [searchTerm]);
+
   return (
-    <div
-      className="flex items-center gap-1 border border-white text-white rounded-2xl px-4 py-2"
-    >
-      {/* Search icon */}
-      <MagnifyingGlassIcon className="w-5 h-5 bg-transparent" />
-      {/* Input */}
-      <input
-        type="text"
-        name="query"
-        title="Search here..."
-        placeholder="Search here..."
-        className="w-full outline-none bg-transparent placeholder:text-white"
-        onChange={handleOnChange}
-      />
+    <div className="relative items-center gap-1 border border-white text-white rounded-2xl px-4 py-2">
+      <div className="flex flex-row">
+        {/* Search icon */}
+        <MagnifyingGlassIcon className="w-5 h-5 my-1 mr-1 bg-transparent" />
+        {/* Input */}
+        <input
+          type="text"
+          name="query"
+          title="Search for a course..."
+          placeholder="Search here..."
+          className="w-full outline-none bg-transparent placeholder:text-white"
+          onChange={handleOnChange}
+        />
+      </div>
+      {/* Dropdown for search results */}
+      {searchTerm && allCourses.length ?
+        <div className="absolute z-10 mt-2 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none border border-black w-full max-h-52 overflow-y-auto no-scrollbar">
+          <div className="py-1">
+            {allCourses.map((course: Course) => (
+              <a href={`/course/${course.courseCode}`} className="hover:bg-gray-200 text-gray-700 block px-4 py-2 text-sm" key={course.courseCode}>{course.courseCode} - {course.title}</a>
+            ))}
+          </div>
+        </div>
+      : undefined}
     </div>
   );
 }
