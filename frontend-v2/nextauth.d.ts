e9408c3ef0ce4aff515d838ae0e030f4cb04af4c
Ahnaf Tazwar <ahnaftazwar368@gmail.com>
2023-07-20 22:08:26 +1000

    add comment for module augmentation

--- frontend-v2/nextauth.d.ts
+++ frontend-v2/nextauth.d.ts
@@ -4,6 +4,7 @@ declare module "next-auth" {
   interface;
 }
 
+// Extends default session and user entities to add access token
 declare module "next-auth" {
   interface User {
     accessToken: string | undefined;
@@ -14,6 +15,7 @@ declare module "next-auth" {
   }
 }
 
+// Extends the default token to add zid and access token support
 declare module "next-auth/jwt" {
   interface JWT {
     zid: string;
