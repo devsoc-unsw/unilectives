8c8216f0340b126d1948080a5907a03e88460005
Aimen <66828989+aimenhamed@users.noreply.github.com>
2023-08-24 20:16:23 +1000

    fix path for apis (#238)
    * fix path for apis
    
    * fix

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -4,9 +4,10 @@ const request = async (
   options?: Record<string, any>,
 ) => {
   const prefix = "/api/v1";
+  const backend = process.env.API_URL ?? "";
   const baseUrl =
     process.env.NODE_ENV !== "development"
-      ? `https://cselectives.staging.csesoc.unsw.edu.au${prefix}${url}`
+      ? `${backend}${prefix}${url}`
       : `http://localhost:3030${prefix}${url}`;
 
   const payload =
@@ -36,24 +37,24 @@ export const validatedReq = async (
   options?: Record<string, any>,
 ) => {
   const prefix = "/api/v1";
+  const backend = process.env.API_URL ?? "";
   const baseUrl =
     process.env.NODE_ENV !== "development"
-      ? `https://cselectives.staging.csesoc.unsw.edu.au${prefix}${url}`
+      ? `${backend}${prefix}${url}`
       : `http://localhost:3030${prefix}${url}`;
-  const payload =
-      {
-        method,
-        headers: {
-          "Content-type": "application/json",
-          token: authToken,
-          zid: zid,
-        },
-        body: method === "GET" ? undefined : JSON.stringify(options)
-      }
+  const payload = {
+    method,
+    headers: {
+      "Content-type": "application/json",
+      token: authToken,
+      zid: zid,
+    },
+    body: method === "GET" ? undefined : JSON.stringify(options),
+  };
   const res = await fetch(baseUrl, { ...payload, cache: "no-store" });
   if (!res.ok) {
     if (res.status === 401) {
-      throw new Error('unauthorised');
+      throw new Error("unauthorised");
     }
     return { errorCode: res.status, errorMessage: res.statusText };
   }
