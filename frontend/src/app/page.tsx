a2b361bcdd718cdbaf3b6a00248e60b5d56bc880
joanna209 <80564580+joanna209@users.noreply.github.com>
2023-09-05 16:14:29 +1000

    feat(frontend): add course list LD on landing page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 import LandingPageContent from "@/components/LandingPageContent/LandingPageContent";
 import navbar from "@/assets/navbar.svg";
 import { Metadata } from "next";
+import { AggregateRating, ItemList, WithContext } from "schema-dts";
+import { get } from "@/utils/request";
+import { Course, Courses } from "@/types/api";
+import { ConstructSignatureDeclaration } from "typescript";
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
@@ -10,10 +14,36 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-export default function Home() {
+export default async function Home() {
+  const res = (await get("/courses")) as Courses;
+  const courses = res.courses;
+  // TODO: fix up client side rendering (have the first 25 courses loaded serverside and subsequent clientside)
+
+  const metaLD: WithContext<ItemList> = {
+    "@context": "https://schema.org",
+    "@type": "ItemList",
+    itemListElement: courses.map((course: Course, index: number) => ({
+      "@type": "ListItem",
+      position: index + 1,
+      item: {
+        "@type": "Course",
+        url: `//www.handbook.unsw.edu.au/undergraduate/courses/${new Date().getFullYear()}/${
+          course.courseCode
+        }`,
+        name: course.title,
+        description: course.description,
+        provider: {
+          "@type": "CollegeOrUniversity",
+          name: "University of New South Wales",
+          sameAs: "https://www.unsw.edu.au/",
+        },
+      },
+    })),
+  };
+
   return (
     <div className="mb-20">
-      {/* Navbar */}
+      {/* Landing page graphic */}
       <div>
         <Image
           src={navbar}
@@ -25,6 +55,10 @@ export default function Home() {
         />
       </div>
       {/* Hero Section */}
+      <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(metaLD) }}
+        />
       <div className="flex flex-row w-full justify-center items-center mt-10">
         <div className="flex flex-row w-5/6 space-y-0 justify-between items-left md:space-y-4 md:flex-col md:items-center">
           <div className="flex flex-col w-full gap-3">
