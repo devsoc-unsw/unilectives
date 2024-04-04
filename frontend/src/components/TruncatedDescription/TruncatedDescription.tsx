119bbd6f87a9b86b0b740ed8d0aa3f6632cf08cc
Maximilian Falco Widjaya <maximilianfalco@gmail.com>
2024-04-04 19:07:24 +1100

    fixed truncated descriptions

--- frontend/src/components/TruncatedDescription/TruncatedDescription.tsx
+++ frontend/src/components/TruncatedDescription/TruncatedDescription.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function TruncatedDescription({
   content,
@@ -9,26 +9,48 @@ export default function TruncatedDescription({
   content: string;
   maxCharacters: number;
 }) {
+  
   const [showFullContent, setShowFullContent] = useState(false);
+  const [dynamicCutOffPoint, setDynamicCutOffPoint] = useState(maxCharacters);
+  const [shortenedContent, setShortenedContent] = useState("");
+  // const shortenedContent =
+  //   content.length < maxCharacters
+  //     ? content
+  //     : `${content.slice(0, maxCharacters)}...`;
 
-  const shortenedContent =
-    content.length < maxCharacters
-      ? content
-      : `${content.slice(0, maxCharacters)}...`;
+  useEffect(() => {
+    if (content.length > maxCharacters) {
+      let newMax = maxCharacters;
+      let index = newMax - 1;
+      while (true) {
+        if (content[index] === ' ' || index === content.length) {
+          newMax = index;
+          break;
+        }
+        newMax++;
+        index++;
+      }
+      setShortenedContent(content.slice(0, newMax) + '...');
+    } else {
+      setShortenedContent(content);
+    }
+  }, [])
 
   return (
     <div>
-      <p className="whitespace-pre-line break-all inline">
+      <p className="whitespace-pre-line text-justify inline">
         {showFullContent ? content : shortenedContent}{" "}
       </p>
-      {content.length > maxCharacters && (
-        <button
-          className="text-unilectives-blue hover:underline"
-          onClick={() => setShowFullContent((prev) => !prev)}
-        >
-          {showFullContent ? "See Less" : "See More"}
-        </button>
-      )}
+      <div className="mt-1">
+        {content.length > maxCharacters && (
+          <button
+            className="text-unilectives-blue hover:underline"
+            onClick={() => setShowFullContent((prev) => !prev)}
+          >
+            {showFullContent ? "See Less" : "See More"}
+          </button>
+        )}
+      </div>
     </div>
   );
 }
