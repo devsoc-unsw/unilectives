d6f07e74990b5aa235165af61fe15965f25ca714
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-05-10 19:38:56 +1000

    install and configure prettier for migration package

--- migration/eslint.config.js
+++ migration/eslint.config.js
@@ -1,10 +1,12 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintConfigPrettier,
 ];
+
