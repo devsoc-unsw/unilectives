8db00f08f94d065d207b7957fb4a3649348b8fd8
Kevin Sutandi <63829516+KevinSutandi@users.noreply.github.com>
2024-06-05 23:57:28 +1000

    Feature:UNI/277 Dark Mode fix for flashing (#299)
    * checkpoint t&c hr
    
    * final designs done
    
    * dark mode border on navbar changed
    
    * provided fix for flashing when refreshing or changing pages in dark mode
    
    * removed console log
    
    ----------

--- frontend/src/components/ThemeComponents/ThemeProvider.tsx
+++ frontend/src/components/ThemeComponents/ThemeProvider.tsx
@@ -10,14 +10,14 @@ type Props = {
 };
 
 export default function ThemeProviderComponent({ children }: Props) {
-  const [mounted, setMounted] = useState<boolean>(false);
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
     setMounted(true);
   }, []);
 
   if (!mounted) {
-    return <>{children}</>;
+    return null;
   }
 
   return (
