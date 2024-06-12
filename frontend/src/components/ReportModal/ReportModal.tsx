b8cef049d324a89838c3df7ef839471b095985bf
Kevin Sutandi <63829516+KevinSutandi@users.noreply.github.com>
2024-06-12 15:24:59 +1000

    feature/uni-277 dark mode: Updated Report Modal to support dark mode (#301)
    * made report modal reflect dark mode changes
    
    ---------

--- frontend/src/components/ReportModal/ReportModal.tsx
+++ frontend/src/components/ReportModal/ReportModal.tsx
@@ -124,7 +124,7 @@ export default function ReportModal({ reviewId }: { reviewId: string }) {
                   leaveFrom="opacity-100 scale-100"
                   leaveTo="opacity-0 scale-95"
                 >
-                  <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-md text-left align-middle shadow-xl transition-all bg-unilectives-modal px-8 py-6 space-y-4 isolate">
+                  <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-md text-left align-middle shadow-xl transition-all bg-unilectives-modal dark:bg-gray-700  px-8 py-6 space-y-4 isolate">
                     {/* Modal title + close button */}
                     <div className="flex justify-between items-center">
                       <Dialog.Title as="h2" className="text-2xl font-bold">
