7344397d9580430fca900641213d1b40bcb74fe0
Christin322 <78027463+Christin322@users.noreply.github.com>
2024-10-04 12:19:20 +1000

    Bugfix/uni 315 text overflow (#318)
    * Fixed overflowing word
    
    ---------

--- backend/src/repositories/course.repository.ts
+++ backend/src/repositories/course.repository.ts
@@ -4,6 +4,8 @@ import {
   CourseCodeSchema,
   CourseSchema,
 } from "../api/schemas/course.schema";
+import e from "express";
+import { Console } from "console";
 
 export class CourseRepository {
   constructor(private readonly prisma: PrismaClient) {}
