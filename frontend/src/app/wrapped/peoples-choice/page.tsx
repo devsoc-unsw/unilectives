2ce5a8464ff263ecd2e509dd1d5af7c39e851e1c
Kevin Sutandi <kevins050324@gmail.com>
2024-10-13 12:21:05 +1100

    page is done but not responsive

--- frontend/src/app/wrapped/peoples-choice/page.tsx
+++ frontend/src/app/wrapped/peoples-choice/page.tsx
@@ -7,6 +7,10 @@ import year from "../../../../public/wrapped-year.svg";
 import WrappedTrophy from "@/assets/wrapped-trophy.png"
 import {useGSAP} from "@gsap/react";
 import {gsap} from "gsap";
+import Link from "next/link";
+import ShareIcon from "@/assets/share-icon.svg"
+import Star from "@/assets/star.svg"
+import BoxComponent from "@/components/Wrapped/PeoplesChoice/BoxComponent";
 
 export default function PeoplesChoicePage() {
   useGSAP(() => {
@@ -20,33 +24,48 @@ export default function PeoplesChoicePage() {
 
   return (
     <PageTransition>
-      <div className='bg-unilectives-pink w-full h-full overflow-scroll'>
+      <div className='bg-unilectives-pink w-full h-full'>
         <div className='flex justify-center mb-12'>
           <Navbar/>
         </div>
-        <div className='grid grid-cols-12 mx-20 mb-12'>
-          <div className='grid cols-span-5'>
+        <div className='grid grid-cols-12 mx-20 mb-28'>
+          <div className='col-span-5'>
             <h1 className='text-8xl font-bold italic text-center text-white font-timesnewroman uppercase'>People&apos;s
               Choice</h1>
-            <div className='relative'>
-              <div className='absolute right-5 top-32'>
+            <div className='relative w-full h-0 pb-[100%]'>
+              <div className='absolute right-0 top-44'>
                 <div className="flex items-center justify-center">
                   <Image
-                    className="year-bg w-[185px] h-[185px]"
+                    className="year-bg w-[175px] h-[175px]"
                     src={year}
                     alt="year"
                   />
                   <p className="absolute text-2xl !text-black font-extrabold">2024</p>
                 </div>
               </div>
-              <Image src={WrappedTrophy} alt={"wrapped trophy"} className='w-96'/>
+              <Image src={WrappedTrophy} alt={"wrapped trophy"} fill
+                     className='max-w-full max-h-full object-contain object-center'/>
             </div>
           </div>
-
+          <div className='col-span-7 ms-10'>
+            <h1 className='text-[112px] text-white font-neuemetana uppercase'>COMP1511</h1>
+            <h3 className='text-4xl font-bold text-white font-neuemetana'>Programming Fundamentals</h3>
+            <Link className='mt-10 text-white flex items-start gap-3 font-semibold underline'
+                  href={'/course/COMP1511'}>
+              <Image src={ShareIcon} alt={"share icon"} className='w-5 h-5'/>
+              Visit Course in Unilectives
+            </Link>
+            <div className='grid grid-cols-2 grid-rows-2 gap-10 mt-10'>
+              <BoxComponent title={"Enjoyment"} rating={"4.7"}/>
+              <BoxComponent title={"Usefulness"} rating={"4.8"}/>
+              <BoxComponent title={"Manageability"} rating={"4.7"}/>
+              <BoxComponent title={"Overall Rating"} rating={"4.7"}/>
+            </div>
+          </div>
         </div>
       </div>
     </PageTransition>
 
   )
-    ;
+
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,79 +2,80 @@
 const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-  darkMode: "class",
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    screens: {
-      // The following screen sizes are converted into css like so
-      // @media (max-width: ...px) { ... }
-      "2xl": { max: "1536px" },
-      xl: { max: "1280px" },
-      lg: { max: "1024px" },
-      md: { max: "768px" },
-      sm: { max: "640px" },
-      xs: { max: "480px" },
-    },
-    extend: {
-      backgroundColor: {
-        "unilectives-bg": "#FDFDFD",
-      },
-      colors: {
-        "unilectives-pink": "#EE82C7",
-        "unilectives-blue": "#1279F2",
-        "unilectives-yellow": "#CCFD51",
-        "unilectives-yellow-hover": "#BFEE49",
-        "unilectives-headings": "#202020",
-        "unilectives-subheadings": "#989898",
-        "unilectives-placeholder": "#606060",
-        "unilectives-tags": "#CCEBF6",
-        "unilectives-card": "#FAFAFA",
-        "unilectives-light-blue": "#84CEE7",
-        "unilectives-purple": "#B789E5",
-        "unilectives-indigo": "#9BADE8",
-        "unilectives-button": "#33ABD1",
-        "unilectives-search": "#9CADE9",
-        "unilectives-modal": "#EEEEEE",
-        "unilectives-icon": "#60A8D8",
-        "unilectives-tags-pink": "#F7D6E0",
-        "unilectives-wrapped-purple": "#5347F5",
-      },
-      boxShadow: {
-        btn: "-0.313rem 0.313rem 0.625rem -0.063rem rgb(0 0 0 / 15%)",
-        "btn-hovered": "0 0.125rem 0.625rem 0 rgb(0 0 0 / 50%)",
-        card: "-0.125rem 0.25rem 0.625rem rgba(0, 0, 0, 0.15)",
-        input: "0 0 0.5rem 0.063rem rgba(0, 0, 0, 0.1);",
-        "review-card": "0px 2px 2px rgba(0, 0, 0, 0.1)",
-      },
-      fontFamily: {
-        custom: ["TT Commons Pro Variable", "Segoe UI", "Arial", "sans-serif"],
-        neuemetana: ["NeueMetana", "sans-serif"],
-        timesnewroman: ["Times New Roman"],
-      },
-      scale: {
-        "-1": "-1",
-      },
-    },
-    listStyleType: {
-      decimal: "decimal",
-      roman: "lower-roman",
-      latin: "upper-Latin",
-      disc: "disc",
-    },
-  },
-  plugins: [
-    plugin(function ({ addBase }) {
-      addBase({
-        img: {
-          "max-width": "none",
+    darkMode: "class",
+    content: ["./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        screens: {
+            // The following screen sizes are converted into css like so
+            // @media (max-width: ...px) { ... }
+            "2xl": {max: "1536px"},
+            xl: {max: "1280px"},
+            lg: {max: "1024px"},
+            md: {max: "768px"},
+            sm: {max: "640px"},
+            xs: {max: "480px"},
         },
-        body: {
-          background: "#FDFDFD",
-          margin: "0",
-          height: "100%",
+        extend: {
+            backgroundColor: {
+                "unilectives-bg": "#FDFDFD",
+            },
+            colors: {
+                "unilectives-pink": "#EE82C7",
+                "unilectives-blue": "#1279F2",
+                "unilectives-yellow": "#CCFD51",
+                "unilectives-yellow-hover": "#BFEE49",
+                "unilectives-headings": "#202020",
+                "unilectives-subheadings": "#989898",
+                "unilectives-placeholder": "#606060",
+                "unilectives-tags": "#CCEBF6",
+                "unilectives-card": "#FAFAFA",
+                "unilectives-light-blue": "#84CEE7",
+                "unilectives-purple": "#B789E5",
+                "unilectives-indigo": "#9BADE8",
+                "unilectives-button": "#33ABD1",
+                "unilectives-search": "#9CADE9",
+                "unilectives-modal": "#EEEEEE",
+                "unilectives-icon": "#60A8D8",
+                "unilectives-tags-pink": "#F7D6E0",
+                "unilectives-wrapped-purple": "#5347F5",
+            },
+            boxShadow: {
+                btn: "-0.313rem 0.313rem 0.625rem -0.063rem rgb(0 0 0 / 15%)",
+                "btn-hovered": "0 0.125rem 0.625rem 0 rgb(0 0 0 / 50%)",
+                card: "-0.125rem 0.25rem 0.625rem rgba(0, 0, 0, 0.15)",
+                input: "0 0 0.5rem 0.063rem rgba(0, 0, 0, 0.1);",
+                "review-card": "0px 2px 2px rgba(0, 0, 0, 0.1)",
+            },
+            fontFamily: {
+                custom: ["TT Commons Pro Variable", "Segoe UI", "Arial", "sans-serif"],
+                neuemetana: ["NeueMetana", "sans-serif"],
+                timesnewroman: ["Times New Roman"],
+                inter: ["Inter", "sans-serif"],
+            },
+            scale: {
+                "-1": "-1",
+            },
         },
-      });
-    }),
-    require("tailwind-scrollbar"),
-  ],
+        listStyleType: {
+            decimal: "decimal",
+            roman: "lower-roman",
+            latin: "upper-Latin",
+            disc: "disc",
+        },
+    },
+    plugins: [
+        plugin(function ({addBase}) {
+            addBase({
+                img: {
+                    "max-width": "none",
+                },
+                body: {
+                    background: "#FDFDFD",
+                    margin: "0",
+                    height: "100%",
+                },
+            });
+        }),
+        require("tailwind-scrollbar"),
+    ],
 };
