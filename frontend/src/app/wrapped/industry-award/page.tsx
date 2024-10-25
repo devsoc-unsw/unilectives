5428817ab84f5685661ebbc9b0fdcdece5e82789
Maximilian Falco Widjaya <maximilianfalco@gmail.com>
2024-10-25 16:24:13 +1100

    completed indutry awards page, need to add responsiveness and dynamic stuff later

--- frontend/src/app/wrapped/industry-award/page.tsx
+++ frontend/src/app/wrapped/industry-award/page.tsx
@@ -1,9 +1,108 @@
 "use client";
 
+import { StarIcon } from "@/components/Wrapped/BoxComponent/StarIcon";
+import { DegreeHatIcon } from "@/components/Wrapped/IndustryAward/DegreeHatIcon";
+import { ShareIcon } from "@/components/Wrapped/IndustryAward/ShareIcon";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import Image from "next/image";
+import year from "../../../../public/wrapped-year.svg";
+
 export default function IndustryAwardPage() {
+  const courseCode = "COMP2511";
+  const courseName = "Object oriented programming";
+
+  const rightTitle = "( INDUSTRY AWARD )";
+  const rating = "4.7";
+
+  const handleVisitCourse = () => {
+    window.open("https://youtube.com", "_blank");
+  }
+
+  useGSAP(() => {
+    gsap.to(".year-bg", {
+      rotation: -360,
+      duration: 8,
+      repeat: -1,
+      ease: "linear",
+    });
+  }, []);
+  
   return (
-    <div>
-      Industry Award Page
+    <div
+      className="w-screen h-screen overflow-hidden font-neuemetana bg-unilectives-wrapped-purple flex justify-center items-center"
+    >
+      <main key="main-frame" className="bg-white w-2/3 h-2/3 rounded-[2rem] p-12 flex gap-10 relative">
+        {/* TODO: Remove hardcoding of rotation and top and left position */}
+        <div className="absolute -top-[60px] -left-[60px] flex items-center justify-center -rotate-[40deg]">
+          <Image
+            className="year-bg w-[200px] h-[200px]"
+            src={year}
+            alt="year"
+          />
+          <p className="absolute text-2xl !text-black font-extrabold">2024</p>
+        </div>
+
+        <section key="left-content" className="w-1/2 flex flex-col">
+          <div className="h-2/3 flex justify-center items-center">
+            <DegreeHatIcon/>
+          </div>
+          <div className="h-1/3 flex flex-col gap-2 justify-center items-center bg-unilectives-wrapped-purple rounded-xl">
+            <p className="text-6xl font-bold">{courseCode}</p>
+            <p>{courseName}</p>
+          </div>
+        </section>
+
+        <section key="right-content" className="w-1/2 flex flex-col gap-8 justify-center items-center">
+          <div className="text-unilectives-wrapped-purple text-4xl font-bold">
+            {rightTitle}
+          </div>
+          <div key="score-table" className="w-[100%] flex flex-col px-2">
+            <div className="text-black flex items-center justify-between p-2 h-12">
+              <div className="font-bold">Enjoyment</div>
+              <div>
+                <div className="flex-grow-0 flex gap-2">
+                  {rating}
+                  <StarIcon className={`w-5 h-5 black`} />
+                </div>
+              </div>
+            </div>
+            <div className="text-black flex items-center justify-between p-2 h-12">
+              <div className="font-bold">Usefulness</div>
+              <div>
+                <div className="flex-grow-0 flex gap-2">
+                  {rating}
+                  <StarIcon className={`w-5 h-5 black`} />
+                </div>
+              </div>
+            </div>
+            <div className="text-black flex items-center justify-between p-2 h-12">
+              <div className="font-bold">Usability</div>
+              <div>
+                <div className="flex-grow-0 flex gap-2">
+                  {rating}
+                  <StarIcon className={`w-5 h-5 black`} />
+                </div>
+              </div>
+            </div>
+            <div className="text-black flex items-center justify-between p-2 h-12">
+              <div className="font-bold text-unilectives-wrapped-purple">Overall</div>
+              <div>
+                <div className="flex-grow-0 flex gap-2">
+                  {rating}
+                  <StarIcon className={`w-5 h-5 black`} />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-4 hover:text-gray-700 cursor-pointer group" onClick={handleVisitCourse}>
+            <ShareIcon className="w-6 h-6 stroke-unilectives-wrapped-purple group-hover:stroke-gray-700"/>
+            <div className="text-unilectives-wrapped-purple text-base underline group-hover:text-gray-700">
+              Visit course on Unilectives
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   )
 }
---
+++ frontend/src/components/Wrapped/IndustryAward/ShareIcon.tsx
@@ -0,0 +1,18 @@
+export function ShareIcon (props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg 
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 25 25" 
+      fill="none" 
+      {...props}
+    >
+      <path 
+        d="M14.5 3.5H21.5V10.5M21.5 15.237V20C21.5 20.3978 21.342 20.7794 21.0607 21.0607C20.7794 21.342 20.3978 21.5 20 21.5H5C4.60218 21.5 4.22064 21.342 3.93934 21.0607C3.65804 20.7794 3.5 20.3978 3.5 20V5C3.5 4.60218 3.65804 4.22064 3.93934 3.93934C4.22064 3.65804 4.60218 3.5 5 3.5H9.5M13.4 11.6L21.05 3.95" 
+        // stroke="#5347F5" 
+        stroke-width="4" 
+        stroke-linecap="round" 
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
