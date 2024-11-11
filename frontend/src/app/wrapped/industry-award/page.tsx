2842ef59680fcfa85fedeab51968374423dc400b
Maximilian Falco Widjaya <maximilianfalco@gmail.com>
2024-11-12 06:04:36 +1100

    industry award page is semi responsive, the page now correctly calls the API and the link to the course page works. need to redefine what industry award is so it can call the correct API routes and need to double check scores and rating labels, there seem to be some mismatch

--- frontend/src/app/wrapped/industry-award/page.tsx
+++ frontend/src/app/wrapped/industry-award/page.tsx
@@ -7,16 +7,30 @@ import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
 import Image from "next/image";
 import year from "../../../../public/wrapped-year.svg";
+import { useEffect, useRef, useState } from "react";
+import { get } from "@/utils/request";
+import { set } from "lodash";
 
 export default function IndustryAwardPage() {
-  const courseCode = "COMP2511";
-  const courseName = "Object oriented programming";
+  const rightSectionRef = useRef<HTMLDivElement>(null);
+  const [rightTitleSize, setRightTitleSize] = useState<string | null>("16px");
+
+  const courseTitleBoxRef = useRef<HTMLDivElement>(null);
+  const [courseTitleSize, setCourseTitleSize] = useState<string | null>("16px");
+  const [courseCodeSize, setCourseCodeSize] = useState<string | null>("24px");
+
+  const [courseCode, setCourseCode] = useState<string | null>(null);
+  const [courseName, setCourseName] = useState<string | null>(null);
 
   const rightTitle = "( INDUSTRY AWARD )";
-  const rating = "4.7";
+  const [overallRating, setOverallRating] = useState<number | null>(null);
+  const [enjoymentRating, setEnjoymentRating] = useState<number | null>(null);
+  const [usefulnessRating, setUsefulnessRating] = useState<number | null>(null);
+  const [usabilityRating, setUsabilityRating] = useState<number | null>(null);
 
+  const [link, setLink] = useState<string>("https://unilectives.devsoc.app/");
   const handleVisitCourse = () => {
-    window.open("https://youtube.com", "_blank");
+    window.open(link, "_blank");
   }
 
   useGSAP(() => {
@@ -26,6 +40,66 @@ export default function IndustryAwardPage() {
       repeat: -1,
       ease: "linear",
     });
+  }, []);
+
+  useEffect(() => {
+    // TODO: Need to properly define what industry award is so that we can fetch the data
+    async function fetchCourseCode() {
+      const code = await get(`/wrapped/course/highest-rated/1`);
+      return code;
+    };
+
+    fetchCourseCode()
+    .then(async (data) => {
+      setCourseCode(data.courseCode);
+      const newLink = `https://unilectives.devsoc.app/course/${data.courseCode}`;
+      setLink(newLink);
+      return get(`/course/${data.courseCode}`);
+    })
+    .then(async (courseData) => {
+      console.log(courseData.course);
+      const data = courseData.course;
+      setCourseName(data.title)
+
+      const enjoyability = Math.round(data.enjoyability * 100) / 100;
+      const usefulness = Math.round(data.usefulness * 100) / 100;
+      const usability = Math.round(data.manageability * 100) / 100;
+      const overall = Math.round(data.overallRating * 100) / 100
+      setEnjoymentRating(enjoyability);
+      setUsefulnessRating(usefulness);
+      setUsabilityRating(usability);
+      setOverallRating(overall);
+    });
+  }, []);
+
+  useEffect(() => {
+    function updateRightSectionFontSize() {
+      if (rightSectionRef.current) {
+        const sectionWidth = rightSectionRef.current.offsetWidth;
+        const newFontSize = sectionWidth * 0.08; 
+        setRightTitleSize(`${newFontSize}px`);
+      }
+    }
+
+    function updateLeftSectionFontSize() {
+      if (courseTitleBoxRef.current) {
+        const sectionWidth = courseTitleBoxRef.current.offsetWidth;
+        const newCourseCodeSize = sectionWidth * 0.135; 
+        setCourseCodeSize(`${newCourseCodeSize}px`);
+
+        const newCourseTitleSize = sectionWidth * 0.04;
+        setCourseTitleSize(`${newCourseTitleSize}px`);
+      }
+    }
+
+    updateRightSectionFontSize();
+    updateLeftSectionFontSize();
+    window.addEventListener('resize', updateRightSectionFontSize);
+    window.addEventListener('resize', updateLeftSectionFontSize);
+    return () => {
+      window.removeEventListener('resize', updateRightSectionFontSize)
+      window.removeEventListener('resize', updateLeftSectionFontSize)
+    }
   }, []);
   
   return (
@@ -47,22 +121,25 @@ export default function IndustryAwardPage() {
           <div className="h-2/3 flex justify-center items-center">
             <DegreeHatIcon/>
           </div>
-          <div className="h-1/3 flex flex-col gap-2 justify-center items-center bg-unilectives-wrapped-purple rounded-xl">
-            <p className="text-6xl font-bold">{courseCode}</p>
-            <p>{courseName}</p>
+          <div ref={courseTitleBoxRef} className="h-1/3 flex flex-col gap-2 justify-center items-center bg-unilectives-wrapped-purple rounded-xl">
+            <p className="text-6xl font-bold" style={{ fontSize: courseCodeSize || "24px" }}>{courseCode}</p>
+            <p style={{ fontSize: courseTitleSize || "16px" }}>{courseName}</p>
           </div>
         </section>
 
-        <section key="right-content" className="w-1/2 flex flex-col gap-8 justify-center items-center">
-          <div className="text-unilectives-wrapped-purple text-4xl font-bold">
-            {rightTitle}
+        <section ref={rightSectionRef} key="right-content" className="w-1/2 flex flex-col gap-8 justify-center items-center">
+          <div
+            className="text-unilectives-wrapped-purple font-bold"
+            style={{ fontSize: rightTitleSize || "16px" }}
+          >
+          {rightTitle}
           </div>
           <div key="score-table" className="w-[100%] flex flex-col px-2">
             <div className="text-black flex items-center justify-between p-2 h-12">
               <div className="font-bold">Enjoyment</div>
               <div>
                 <div className="flex-grow-0 flex gap-2">
-                  {rating}
+                  {enjoymentRating}
                   <StarIcon className={`w-5 h-5 black`} />
                 </div>
               </div>
@@ -71,16 +148,17 @@ export default function IndustryAwardPage() {
               <div className="font-bold">Usefulness</div>
               <div>
                 <div className="flex-grow-0 flex gap-2">
-                  {rating}
+                  {usefulnessRating}
                   <StarIcon className={`w-5 h-5 black`} />
                 </div>
               </div>
             </div>
             <div className="text-black flex items-center justify-between p-2 h-12">
+              {/* TODO: Need to double check, this might need to be manageability actually */}
               <div className="font-bold">Usability</div>
               <div>
                 <div className="flex-grow-0 flex gap-2">
-                  {rating}
+                  {usabilityRating}
                   <StarIcon className={`w-5 h-5 black`} />
                 </div>
               </div>
@@ -89,7 +167,7 @@ export default function IndustryAwardPage() {
               <div className="font-bold text-unilectives-wrapped-purple">Overall</div>
               <div>
                 <div className="flex-grow-0 flex gap-2">
-                  {rating}
+                  {overallRating}
                   <StarIcon className={`w-5 h-5 black`} />
                 </div>
               </div>
