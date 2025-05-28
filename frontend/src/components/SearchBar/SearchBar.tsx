d00324943423756750b692b871c2e5e4263ed918
Rokika Kh <rokika.kh1@gmail.com>
2025-05-28 21:58:16 +1000

    adding ctf flag

--- frontend/src/components/SearchBar/SearchBar.tsx
+++ frontend/src/components/SearchBar/SearchBar.tsx
@@ -13,6 +13,10 @@ export default function SearchBar({
 
   const handleOnChange = useCallback(
     debounce((event: ChangeEvent<HTMLInputElement>) => {
+      // Check if user entered the secret phrase
+      if (event.target.value.trim().toLowerCase() === "i'm looking for a flag!") {
+        alert("Congrats! You found the flag: RCR{leave_a_review}!");
+      }
       onSearchChange(event.target.value.trim().replaceAll(" ", "%20"));
     }, 300),
     []
