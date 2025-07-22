599dc71dd7f02b0edba20e19c12d02057dea0be7
alecliu1204 <111400188+alecliu1204@users.noreply.github.com>
2025-07-22 14:07:15 +1000

    merged staging changes (#370)
    * Update code to match main
    
    * Clean reset: make develop code match main exactly
    
    * Light and Dark mode Colour scheme changes (#360)
    
    added visual improvements to CourseCard, Navbar, ReviewCard and tailwind.config.js
    
    * added clickable course code for bookmarked reviews  (#364)
    
    * added link icon and clickable course code to bookmarked reviews
    
    * Make bookmarked review titles clickable, added link icon
    
    * added clickable course code to card view of bookmarked reviews
    
    ---------
    
    Co-authored-by: Annelise Koh <afekoh1559@gmail.com>
    
    * fixed bug for card/list toggle when there were no user reviews/bookmarked reviews (#362)
    
    * colour changes  (#361)
    
    * Update code to match main
    
    * Clean reset: make develop code match main exactly
    
    * Light and Dark mode Colour scheme changes (#360)
    
    added visual improvements to CourseCard, Navbar, ReviewCard and tailwind.config.js
    
    ---------
    
    Co-authored-by: Auston Yang <163545426+ajzombie123@users.noreply.github.com>
    
    * fixed card/list toggle only for when there are user reviews and user bookmarked reviews
    
    * removed extra file
    
    ---------
    
    Co-authored-by: alecliu1204 <111400188+alecliu1204@users.noreply.github.com>
    Co-authored-by: Auston Yang <163545426+ajzombie123@users.noreply.github.com>
    Co-authored-by: Lyn Duong <z5585295@ad.unsw.edu.au>
    
    * added hover colouring for bookmarked reviews (#363)
    
    * colour changes  (#361)
    
    * Update code to match main
    
    * Clean reset: make develop code match main exactly
    
    * Light and Dark mode Colour scheme changes (#360)
    
    added visual improvements to CourseCard, Navbar, ReviewCard and tailwind.config.js
    
    ---------
    
    Co-authored-by: Auston Yang <163545426+ajzombie123@users.noreply.github.com>
    
    * added hover colouring to bookmarked cards
    
    ---------
    
    Co-authored-by: alecliu1204 <111400188+alecliu1204@users.noreply.github.com>
    Co-authored-by: Auston Yang <163545426+ajzombie123@users.noreply.github.com>
    Co-authored-by: Lyn Duong <z5585295@ad.unsw.edu.au>
    
    * moved link icon to the right of course code and made icon smaller (#365)
    
    * added link icon and clickable course code to bookmarked reviews
    
    * Make bookmarked review titles clickable, added link icon
    
    * added clickable course code to card view of bookmarked reviews
    
    * made link icon smaller and moved it to the right of course code
    
    ---------
    
    Co-authored-by: Annelise Koh <afekoh1559@gmail.com>
    Co-authored-by: alecliu1204 <111400188+alecliu1204@users.noreply.github.com>
    
    * Feature/filter and reviews (#368)
    
    * Added animation to modal popup for Filter
    
    * colour changes  (#361)
    
    * Update code to match main
    
    * Clean reset: make develop code match main exactly
    
    * Light and Dark mode Colour scheme changes (#360)
    
    added visual improvements to CourseCard, Navbar, ReviewCard and tailwind.config.js
    
    ---------
    
    Co-authored-by: Auston Yang <163545426+ajzombie123@users.noreply.github.com>
    
    * Fixed filter animation and padding
    
    * Fixed text only review always
    
    ---------
    
    Co-authored-by: alecliu1204 <111400188+alecliu1204@users.noreply.github.com>
    Co-authored-by: Auston Yang <163545426+ajzombie123@users.noreply.github.com>
    
    * changed dimensions of modal and made centered in viewport  (#369)
    
    changed dimensions of modal and made centered in viewport excluding navbar
    
    ---------
    
    Co-authored-by: Auston Yang <163545426+ajzombie123@users.noreply.github.com>
    Co-authored-by: annelise_k <afekoh@icloud.com>
    Co-authored-by: Annelise Koh <afekoh1559@gmail.com>
    Co-authored-by: l-duong05 <176846912+l-duong05@users.noreply.github.com>
    Co-authored-by: Lyn Duong <z5585295@ad.unsw.edu.au>
    Co-authored-by: Xx358hffy <150639855+Xx358hffy@users.noreply.github.com>

--- frontend/src/components/FilterModal/FilterModal.tsx
+++ frontend/src/components/FilterModal/FilterModal.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Dropdown from "../Dropdown/Dropdown";
 import { useState } from "react";
 import { Dialog } from "@headlessui/react";
+import { Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/outline";
 import { Course } from "@/types/api";
@@ -105,15 +106,6 @@ export default function FilterModal({
       }
     });
 
-    // Show secret message if specific filters are selected
-    if (
-      selectedFaculties.includes("Arts") &&
-      selectedTerms.includes("-1") &&
-      selectedTerms.includes("-2")
-    ) {
-      alert("Alss aol zlhyjo ihy...");
-    }
-
     setFilters({ faculties: selectedFaculties, terms: selectedTerms });
 
     setOpen(false);
@@ -157,93 +149,108 @@ export default function FilterModal({
 
   return (
     <>
-      {/* filter button */}
-      <div className="mt-4 min-w-[140px] xs:min-w-1/2">
-        <button
-          className="relative flex items-center dark:text-white dark:bg-gray-900 justify-between gap-2 w-full cursor-pointer py-2 px-4 text-left text-[#989898] border border-unilectives-subheadings rounded-md shadow-review-card aria-expanded:border-b-transparent aria-expanded:rounded-b-none"
-          onClick={() => {
-            setOpen(true);
-          }}
-        >
-          Filter
-          <AdjustmentsHorizontalIcon className="w-4 h-4 rounded-full dark:text-white text-black" />
-        </button>
-      </div>
-
-      {/* filter dialog */}
-      <Dialog open={open} onClose={handleClose}>
-        {/* the blurred backdrop */}
-        <div
-          className="fixed inset-0 bg-black/30 backdrop-blur-sm z-10"
-          aria-hidden="true"
-        />
-
-        {/* Full-screen container to center the panel */}
-        <div className="fixed inset-0 flex w-screen items-center justify-center pl-20 z-10 ">
-          {/* The actual dialog panel  */}
-          <Dialog.Panel className="mx-auto max-w-sm p-8 rounded dark:bg-gray-900 bg-white flex flex-col xs:w-full">
-            <button className="w-6 h-6 place-self-end" onClick={handleClose}>
-              <XMarkIcon />
-            </button>
-            <Dialog.Title className="text-2xl dark:text-white font-bold mb-4 text-unilectives-headings">
-              Filter by:
-            </Dialog.Title>
-            <Dialog.Description className="text-xl font-semibold mb-2 ">
-              Faculty
-            </Dialog.Description>
-            {/* display the faculty tags */}
-            <div className="flex flex-wrap mb-4">
-              {faculties.map((faculty, index) => {
-                return styledFilterButton(
-                  "faculty",
-                  index,
-                  facultiesCheckedState[index],
-                  faculty,
-                );
-              })}
-            </div>
-            <Dialog.Description className="text-xl font-semibold mb-2">
-              Term
-            </Dialog.Description>
-            {/* display the term tags */}
-            <div className="sm:hidden flex flex-wrap mb-4">
-              {terms.map((term, index) => {
-                return styledFilterButton(
-                  "term",
-                  index,
-                  termsCheckedState[index],
-                  term,
-                );
-              })}
-            </div>
-            <div className="sm:flex hidden flex-wrap mb-4 ">
-              {termsShortened.map((term, index) => {
-                return styledFilterButton(
-                  "term",
-                  index,
-                  termsCheckedState[index],
-                  term,
-                );
-              })}
-            </div>
-
-            <div className="flex justify-between mt-4 xs:flex-col xs:gap-4">
-              <button
-                className="flex items-center justify-center xs:w-full dark:hover:border-gray-900 gap-1 px-4 py-2 text-unilectives-button border-2 border-unilectives-button rounded-md hover:bg-unilectives-icon/95 hover:border-unilectives-icon/95 hover:text-white hover:border-white font-bold disabled:opacity-50"
-                onClick={handleClearAll}
-              >
-                Clear All
-              </button>
-              <button
-                className="flex items-center justify-center w-1/3 xs:w-full gap-1 px-4 py-2 bg-unilectives-button text-white rounded-md hover:bg-unilectives-icon/95 font-bold disabled:opacity-50"
-                onClick={handleApply}
-              >
-                Apply
-              </button>
-            </div>
-          </Dialog.Panel>
+      <div>
+        {/* filter button */}
+        <div className="mt-4 min-w-[140px] xs:min-w-1/2">
+          <button
+            className="relative flex items-center dark:text-white dark:bg-gray-900 justify-between gap-2 w-full cursor-pointer py-2 px-4 text-left text-[#989898] border border-unilectives-subheadings rounded-md shadow-review-card aria-expanded:border-b-transparent aria-expanded:rounded-b-none"
+            onClick={() => {
+              setOpen(true);
+            }}
+          >
+            Filter
+            <AdjustmentsHorizontalIcon className="w-4 h-4 rounded-full dark:text-white text-black" />
+          </button>
         </div>
-      </Dialog>
+
+        {/* filter dialog */}
+        <Transition show={open}>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            className="fixed inset-0 z-10"
+          >
+            {/* Backdrop */}
+            <div className="fixed inset-0 bg-black/30 backdrop-blur-sm" />
+            {/* Modal content wrapper */}
+            <div className="fixed left-20 right-0 top-0 bottom-0 flex items-center justify-center p-4">
+              <Transition.Child
+                enter="transition-transform ease-out duration-700"
+                enterFrom="opacity-0 scale-90"
+                enterTo="opacity-100 scale-100"
+                leave="transition-transform ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="mx-auto w-full max-w-[350px] p-6 rounded dark:bg-gray-900 bg-white flex flex-col">
+                  <button
+                    className="w-6 h-6 place-self-end"
+                    onClick={handleClose}
+                  >
+                    <XMarkIcon />
+                  </button>
+                  <Dialog.Title className="text-2xl dark:text-white font-bold mb-4 text-unilectives-headings">
+                    Filter by:
+                  </Dialog.Title>
+                  <Dialog.Description className="text-xl font-semibold mb-2 ">
+                    Faculty
+                  </Dialog.Description>
+                  {/* display the faculty tags */}
+                  <div className="flex flex-wrap mb-4">
+                    {faculties.map((faculty, index) => {
+                      return styledFilterButton(
+                        "faculty",
+                        index,
+                        facultiesCheckedState[index],
+                        faculty,
+                      );
+                    })}
+                  </div>
+                  <Dialog.Description className="text-xl font-semibold mb-2">
+                    Term
+                  </Dialog.Description>
+                  {/* display the term tags */}
+                  <div className="sm:hidden flex flex-wrap mb-4">
+                    {terms.map((term, index) => {
+                      return styledFilterButton(
+                        "term",
+                        index,
+                        termsCheckedState[index],
+                        term,
+                      );
+                    })}
+                  </div>
+                  <div className="sm:flex hidden flex-wrap mb-4 ">
+                    {termsShortened.map((term, index) => {
+                      return styledFilterButton(
+                        "term",
+                        index,
+                        termsCheckedState[index],
+                        term,
+                      );
+                    })}
+                  </div>
+
+                  <div className="flex justify-between mt-4 xs:flex-col xs:gap-4">
+                    <button
+                      className="flex items-center justify-center xs:w-full dark:hover:border-gray-900 gap-1 px-4 py-2 text-unilectives-button border-2 border-unilectives-button rounded-md hover:bg-unilectives-icon/95 hover:border-unilectives-icon/95 hover:text-white hover:border-white font-bold disabled:opacity-50"
+                      onClick={handleClearAll}
+                    >
+                      Clear All
+                    </button>
+                    <button
+                      className="flex items-center justify-center w-1/3 xs:w-full gap-1 px-4 py-2 bg-unilectives-button text-white rounded-md hover:bg-unilectives-icon/95 font-bold disabled:opacity-50"
+                      onClick={handleApply}
+                    >
+                      Apply
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </Dialog>
+        </Transition>
+      </div>
     </>
   );
 }
