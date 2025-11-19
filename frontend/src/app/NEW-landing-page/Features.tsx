dffbe61a6d0dc1dc7a1ccc9881dfd8541238bd0a
William Lu <williamlu2016@gmail.com>
2025-11-19 16:01:18 +1100

    Created Features Page and left svg files in Assets

--- frontend/src/app/NEW-landing-page/Features.tsx
+++ frontend/src/app/NEW-landing-page/Features.tsx
@@ -1,31 +1,48 @@
+import Image from "next/image";
+import feature_box_one from "../../assets/features/feature_box_one.svg"
+import feature_box_two from "../../assets/features/feature_box_two.svg"
+import feature_box_three from "../../assets/features/feature_box_three.svg"
+
+
+
 export default function Features() {
   return (
-    <div className="bg-[#EDEEEF] py-16 font-roboto">
+    <div className="bg-[#EDEEEF] py-16 font-roboto bg-white">
       <div className="container mx-auto px-4">
         <h2 className="text-4xl font-normal text-center mb-12 ">Our Features</h2>
 
         <div className="flex flex-row md:flex-row justify-center items-center md:items-start space-y-8 md:space-y-0 md:space-x-8">
-          <div className="bg-white rounded-xl shadow-md p-8 w-[398px] h-[483px]">
-            <h3 className="text-xl font-semibold mb-4">Comprehensive Course Reviews</h3>
-            <p className="text-black">
-              Real student insights on difficulty, enjoyment, and teaching quality – all in one place.
-            </p>
+          <div className="flex-1">
+            <Image
+                src={feature_box_one}
+                alt="Comprehensive Course Reviews"
+                width={398}
+                height={488}
+                className="mb-4"
+              />
           </div>
 
           {/* Feature Box 2 (Using responsive Tailwind classes) */}
-          <div className="bg-white rounded-xl shadow-md p-8 flex-1">
-            <h3 className="text-xl font-semibold mb-4">Review Courses</h3>
-            <p className="text-black">
-              Share your experience to help fellow students make smarter study choices.
-            </p>
+          <div className="flex-1">
+            <Image
+                src={feature_box_two}
+                alt="Feature Box 2"
+                width={398}
+                height={488}
+                className="mb-4"
+              />
+
           </div>
 
           {/* Feature Box 3 (Using responsive Tailwind classes) */}
-          <div className="bg-white rounded-xl shadow-md p-8 flex-1">
-            <h3 className="text-xl font-semibold mb-4">Your Account</h3>
-            <p className="text-black">
-              Save favorite courses, submit reviews, and personalise your experience.
-            </p>
+          <div className="flex-1">
+          <Image
+                src={feature_box_three}
+                alt="Feature Box 2"
+                width={398}
+                height={488}
+                className="mb-4"
+              />
           </div>
         </div>
       </div>
